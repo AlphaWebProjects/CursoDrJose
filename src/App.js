9149e284203e19c329b1d9283a274734abc1dbf9
Pedro Pereira <plp.leoncio@gmail.com>
2024-03-20 16:21:47 -0300

    feat: reestruturação de alguns arquivos + instalação de algumas libs + estilização do login e cadastro

--- src/App.js
+++ src/App.js
@@ -2,16 +2,35 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import LandingPage from './Pages/LandingPage';
 import NotFound from './Pages/NotFound';
+import Auth from './Pages/Auth';
+import { ToastContainer } from 'react-toastify';
+import { UserProvider } from './context/UserContext';
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<LandingPage />} />
-        <Route path="*" element={<NotFound />} />
-      </Routes>
-    </Router>
-    
+    <UserProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+
+        <ToastContainer 
+          position="top-right"
+          autoClose={3500}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+
+      </Router>
+    </UserProvider>
   );
 }
 
