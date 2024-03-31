d8cbd15f15c182e842c86d676ffb0a55527f5583
Pedro <malpvpandtextures@gmail.com>
2024-03-31 20:25:33 -0300

    fix updates

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
-import fotoAlunos from '../img/fotoAlunos.jpeg';
-import React, { Component } from 'react';
-import video from "../img/videoViaAerea.mp4";
+import React, { useState, useEffect } from 'react';
 
 function Home() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsVisible(true);
+    }, 50); // Altera a opacidade após 1 segundo
+  }, []);
   const handleHeaderClick = () => {
     const totalHeight = document.body.scrollHeight;
     const scrollToHeight = totalHeight * 0.5; // Rolar para baixo 50% da altura total da página
@@ -14,22 +19,16 @@ function Home() {
     <Container>
       <MainContentHome>
         <CenterContent>
-          <Left>
-            <h1><span style={{ color: '#158A7A' }}>Domine o Essencial:<br></br> Suporte no Manejo da Via Aérea</span></h1>
+          <Left isVisible={isVisible}>
+            <h1><span style={{ color: '#158A7A' }}>Domine o Essencial:<br /> Suporte no Manejo da Via Aérea</span></h1>
             <p>
               Descubra o poder de <span style={{ color: '#158A7A' }}> salvar vidas</span> com confiança! Nosso curso de 
               <span style={{ color: '#158A7A' }}> Suporte Integral no Manejo da Via Aérea</span> é a chave para a excelência na prestação de cuidados médicos. 
               Projetado para profissionais de saúde que buscam dominar as <span style={{ color: '#158A7A' }}>técnicas vitais de intervenção em situações críticas</span>, 
               este curso oferece uma abordagem abrangente, desde os fundamentos até as práticas avançadas
             </p>
-            <Button onClick={() => handleHeaderClick()}>Saiba Mais</Button>
+            <Button  onClick={() => handleHeaderClick()} >Saiba Mais</Button>
           </Left>
-          <Right>
-          <video width="292" height="515" controls muted style={{ boxShadow: '0 0 50px rgba(21, 138, 122, 0.5)', borderRadius:'20px' }}>
-            <source src={video} type="video/mp4" />
-            Seu navegador não suporta o elemento de vídeo.
-          </video>
-          </Right>
         </CenterContent>
       </MainContentHome>
     </Container>
@@ -38,38 +37,42 @@ function Home() {
 
 export default Home;
 
-  
-  const Container = styled.div`
-    width: 100%;
-    height: 90%;
-    margin-top:120px;
-    background-image: url(${fotoAlunos});
-    background-size: cover;
-    background-position: center;
+const Container = styled.div`
+  width: 100%;
+  height: 90%;
+  margin-top: 120px;
+  background-image: url('https://saocamilo-sp.br/assets/uploads/a11.png');
+  background-size: cover;
+  background-position: center;
 `;
- const MainContentHome= styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    padding: 5%;
 
- `
- const Left = styled.div`
-  width: 50%;
+const MainContentHome = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding: 5%;
+`;
+
+const Left = styled.div`
+  width: 100%;
   height: 100%;
   border-radius: 20px;
   box-shadow: rgba(0, 0, 0, 0.45) 25px 0px 20px -20px;
   display: flex;
   text-align: center;
   flex-direction: column;
+  justify-content: center;
   align-items: center;
-  padding:5%;
-  p{
+  padding: 5%;
+  opacity: ${({ isVisible }) => (isVisible ? '1' : '0')};
+  transition: opacity 1s ease-in-out; /* Transição da opacidade em 1 segundo */
+  p {
     font-size: 18px;
     font-weight: 500;
   }
- `
- const Button = styled.button`
+`;
+
+const Button = styled.button`
   background-color: #158A7A;
   color: #fff;
   border: none;
@@ -78,7 +81,7 @@ export default Home;
   padding: 10px 20px;
   cursor: pointer;
   margin-top: 50px;
-  transition: transform 0.3s, box-shadow 0.3s,border-radius 0.3s;
+  transition: transform 0.3s, box-shadow 0.3s, border-radius 0.3s;
   &:hover {
     transform: scale(1.05);
     border-radius: 20px;
@@ -86,20 +89,11 @@ export default Home;
   }
 `;
 
- const Right = styled.div`
-  width: 50%;
+const CenterContent = styled.div`
+  width: 100%;
   height: 100%;
-  border-radius: 10px;
   display: flex;
-  justify-content: center;
-  align-items: center;
- `
-  const CenterContent = styled.div`
-    width: 100%;
-    height: 100%;
-    display: flex;
-    border-radius: 20px;
-    box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
-    background-color: rgba(255, 255, 255, 0.9);
-  `
- 
+  border-radius: 20px;
+  box-shadow: rgb(38, 57, 77) 0px 20px 30px -10px;
+  background-color: rgba(255, 255, 255, 0.9);
+`;
--- src/Components/SobreCurso.js
+++ src/Components/SobreCurso.js
@@ -1,82 +1,148 @@
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import doctor2 from '../img/FotosAlunos1.jpeg';
-function SobreCurso() {
+import img1 from '../img/img_curso1.png';
+import img2 from '../img/img_curso2.png';
+import img3 from '../img/img_curso3.png';
 
-    const handleHeaderClick = (index) => {
-        const totalHeight = document.body.scrollHeight;
-        const scrollToHeight = (totalHeight * 0.94); // Rolar para baixo 30% da altura total da página
-        window.scrollTo({ top: scrollToHeight, behavior: 'smooth' });
+function SobreCurso() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+      const elementOffset = document.getElementById('mainContentCurso').offsetTop;
+      const isVisible = scrollTop > elementOffset - window.innerHeight + 200;
+      setIsVisible(isVisible);
     };
-    return (
-      <Container>
-        <MainContentCurso>
-            <CenterContent>
-                    <h1>Sobre o curso</h1>
-                    <p>
-                      Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-                       Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
-                       when an unknown printer took a galley of type and scrambled it to make a type specimen book.
-                       It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
-                        It was popularised in the 1960s with the release of Letraset sheets containing
-                         Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum
-                      </p>
-                      <Button onClick={() => handleHeaderClick()}>Faça sua Inscrição</Button>
-            </CenterContent>
-        </MainContentCurso>
-      </Container>
-    );
-  }
-  
-  export default SobreCurso;
-  
-  const Container = styled.div`
-    width: 100%;
-    height: 90%;
-    background-image: url(${doctor2});
-    background-size: cover;
-    background-position: center top 60%
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
+  return (
+    <Container>
+      <MainContentCurso id="mainContentCurso">
+        <CenterContent>
+          <h1>Sobre o curso</h1>
+          <InfoContainer>
+            <LeftInfos isVisible={isVisible}>
+              <h2>Um treinamento para vida</h2>
+              <ul>
+                <li>Treinamento para manejo da via aérea</li>
+                <li>Desenvolvido por especialistas da USP</li>
+                <li>Voltado para a realidade médica do dia a dia</li>
+                <li>Fundamentado em casos reais</li>
+                <li>Gerenciamento seguro da via aérea avançada</li>
+                <li>Manejo refinado de condução de fármacos</li>
+              </ul>
+            </LeftInfos>
+            <RightInfos isVisible={isVisible}>
+              <div>
+                <img src={img1} width="150" height="150" alt="Logo" />
+                <h2>Prático</h2>
+                <h3>Treinamento prático com casos clínicos reais</h3>
+              </div>
+              <div>
+                <img src={img2} width="150" height="150" alt="Logo" />
+                <h2>Teórico</h2>
+                <h3>Conteúdo didático sedimentado através de uma metodologia exclusiva</h3>
+              </div>
+              <div>
+                <img src={img3} width="150" height="150" alt="Logo" />
+                <h2>Qualidade Usp</h2>
+                <h3>Equipe formada no maior centro de excelência médica do país.</h3>
+              </div>
+            </RightInfos>
+          </InfoContainer>
+        </CenterContent>
+      </MainContentCurso>
+    </Container>
+  );
+}
+
+export default SobreCurso;
+
+const InfoContainer = styled.div`
+  display: flex;
+  width: 90%;
+  height: 100%;
+  justify-content: space-between;
 `;
 
- const MainContentCurso = styled.div`
-        width: 100%;
-        height: 100%;
-        display: flex;
-        padding: 5%;
-        
- `
- const CenterContent = styled.div`
-        width: 100%;
-        height: 100%;
-        display: flex;
-        flex-direction: column;
-        justify-content: space-around;
-        align-items: center;
-        border-radius: 20px;
-        background-color: rgba(255, 255, 255, 0.9);
-        box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
-        padding:5%;
-        h1{
-          color:#158A7A;
-          font-size: 40px;
-        }
-         p{
-          width:80%;
-          font-size: 20px;
-         }
-`
-const Button = styled.button`
-background-color: #158A7A;
-color: #fff;
-border: none;
-font-size: 20px;
-border-radius: 10px;
-padding: 10px 20px;
-cursor: pointer;
-margin-top: 50px;
-transition: transform 0.3s, box-shadow 0.3s,border-radius 0.3s;
-&:hover {
-  transform: scale(1.05);
-  box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.3);
+const LeftInfos = styled.div`
+  width: 45%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  opacity: ${({ isVisible }) => (isVisible ? '1' : '0')};
+  transition: opacity 0.5s ease-in-out;
+  li {
+    margin-top: 10px;
+  }
+`;
+
+const RightInfos = styled.div`
+  display: flex;
+  width: 45%;
+  justify-content: space-between;
+  opacity: ${({ isVisible }) => (isVisible ? '1' : '0')};
+  transition: opacity 0.5s ease-in-out;
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 30%;
+  }
+  h2 {
+    font-size: 20px;
+  }
+  h3{
+    text-align:center;
+    font-weight: 500;
+    font-size: 15px;
+  }
+  p {
+    font-size: 10px;
+    text-align: center;
+  }
+`;
+
+const Container = styled.div`
+  width: 100%;
+  height: 90%;
+  background-image: url(${doctor2});
+  background-size: cover;
+  background-position: center top 60%;
+`;
+
+const MainContentCurso = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  padding: 5%;
+`;
+
+const CenterContent = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
   border-radius: 20px;
-}
-`
+  background-color: rgba(255, 255, 255, 0.95);
+  box-shadow: rgba(0, 0, 0, 0.35) 0px 5px 15px;
+  padding: 5%;
+  h1 {
+    color: #158a7a;
+    font-size: 40px;
+  }
+  p {
+    width: 80%;
+    font-size: 20px;
+  }
+`;
